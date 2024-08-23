c71bce151b7f5a380ea988e7720737f85a80afba
Kyan Cox <kyan.cox@gmail.com>
2024-08-23 15:24:46 -0400

    Rough responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { ModeToggle } from './DarkMode'
-import { Menu, X } from 'lucide-react' 
+import { Menu, X } from 'lucide-react'
 import {
     Sheet,
     SheetClose,
@@ -12,7 +12,7 @@ import {
     SheetHeader,
     SheetTitle,
     SheetTrigger,
-  } from "@/components/ui/sheet"
+} from "@/components/ui/sheet"
 
 const Navbar = () => {
     const [isVisible, setIsVisible] = useState(true)
@@ -45,43 +45,50 @@ const Navbar = () => {
     ]
 
     return (
-        <nav className={`z-50 p-4 border-b shadow-2xl sticky top-0 transition-transform duration-300 bg-background ${isVisible ? 'translate-y-0' : (isMenuOpen ? '' : '-translate-y-full')}`}>
-            <div className='flex items-center justify-between'>
-                <Link href='#home' className=' text-2xl font-semibold hover:text-slate-400 transition duration-300'>Kyan Cox</Link>
+        <>
+            <nav className={`hidden md:block z-50 p-4 border-b shadow-2xl sticky top-0 transition-transform duration-300 bg-background ${isVisible ? 'translate-y-0' : (isMenuOpen ? '' : '-translate-y-full')}`}>
+                <div className='flex items-center justify-between'>
+                    <Link href='#home' className=' text-2xl font-semibold hover:text-slate-400 transition duration-300'>Kyan Cox</Link>
 
-                {/* Menu for medium screens and above */}
-                <div className='md:flex items-center justify-center space-x-5 hidden'>
-                    {buttons.map(({ name, route }) => (
-                        <Link key={name} href={route} className='text-lg font-semibold hover:text-slate-400 hover:-translate-y-0.5 transition duration-200'>{name}</Link>
-                    ))}
-                    <ModeToggle />
-                </div>
-
-                {/* Hamburger menu for smaller screens */}
-                <SheetTrigger className='flex md:hidden'>
-                    <button onClick={() => setIsMenuOpen(!isMenuOpen)} className='p-2'>
-                        <Menu size={24} />
-                    </button>
-                </SheetTrigger>
-            </div>
-
-            {/* Side menu for smaller screens */}
-            {<div className={`fixed inset-y-0 right-0 w-1/2 bg-slate-800 h-screen shadow-lg p-4 transform transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`} style={{ overflow: 'hidden' }}>
-                <div className='flex flex-col items-end justify-between space-y-4 h-full'>
-                    <button onClick={() => setIsMenuOpen(!isMenuOpen)} className='p-2'>
-                        <X size={24} />
-                    </button>
-                    <div className='flex flex-col'>
+                    {/* Menu for medium screens and above */}
+                    <div className='md:flex items-center justify-center space-x-5 hidden'>
                         {buttons.map(({ name, route }) => (
-                            <Link key={name} href={route} className='text-lg font-semibold hover:text-slate-400 transition duration-200' onClick={() => setIsMenuOpen(false)}>{name}</Link>
+                            <Link key={name} href={route} className='text-lg font-semibold hover:text-slate-400 hover:-translate-y-0.5 transition duration-200'>{name}</Link>
                         ))}
-                    </div>
-                    <div className='mt-auto'>
                         <ModeToggle />
                     </div>
+
                 </div>
-            </div>}
-        </nav>
+            </nav>
+            <Sheet>
+                <nav className={`flex md:hidden items-center justify-between z-50 p-4 border-b shadow-2xl sticky top-0 transition-transform duration-300 bg-background ${isVisible ? 'translate-y-0' : (isMenuOpen ? '' : '-translate-y-full')}`}>
+                    <Link href='#home' className=' text-2xl font-semibold hover:text-slate-400 transition duration-300'>Kyan Cox</Link>
+                    <SheetTrigger>
+                        <Menu size={24} />
+                    </SheetTrigger>
+                </nav>
+                <SheetContent className="w-1/2 flex flex-col justify-between">
+
+                   <div className='flex items-center justify-end'>
+                     <SheetClose className='border p-1 rounded-md' >
+                         <X size={24} />
+                     </SheetClose>
+                   </div>
+
+                    <div className='flex flex-col items-center justify-center space-y-4'>
+                        {buttons.map(({ name, route }) => (
+                            <Link key={name} href={route} className='text-xl font-semibold hover:text-slate-400 transition duration-200' onClick={() => setIsMenuOpen(false)}>{name}</Link>
+                        ))}
+                    </div>
+                    
+                    <div className=' flex items-center justify-end'>
+                        <ModeToggle />
+                    </div>
+                </SheetContent>
+
+
+            </Sheet>
+        </>
     )
 }
 
