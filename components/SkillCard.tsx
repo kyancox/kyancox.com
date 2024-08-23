301f9cac8c86a5a9c564addb825509b56750f936
Kyan Cox <kyan.cox@gmail.com>
2024-08-23 16:00:26 -0400

    Minor mobile changes

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -8,9 +8,9 @@ interface SkillCardProps {
 
 const SkillCard = ({ title, skills }: SkillCardProps) => {
     return (
-        <div className="mx-4 my-4 md:w-2/3 lg:w-1/2 xl:w-1/4 xl:min-h-96 xl:min-w-96 rounded-2xl border-foreground border-2 shadow-lg p-4">
+        <div className="mx-4 my-4 md:w-2/3 lg:w-1/2 xl:w-1/4 xl:min-h-96 xl:min-w-96 rounded-2xl border-foreground border md:border-2 shadow-lg p-4">
             <p className='text-3xl h-full text-center font-semibold mb-2 mx-2 text-foreground'>{title}</p>
-            <div className='flex-wrap flex flex-row items-center justify-center mx-2'>
+            <div className='flex-wrap flex flex-row items-center justify-center md:mx-2'>
                 {Object.entries(skills).map(([key, value]) => (
                     <div className='py-1 px-3  rounded-full m-1.5 bg-foreground hover:opacity-60 transition duration-50 flex flex-row items-center justify-center space-x-1.5' key={key}>
                         <Image
