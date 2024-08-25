1f5fbb07b2666aa6e78eb4e5ffff6a257a218234
Kyan Cox <kyan.cox@gmail.com>
2024-08-25 14:34:56 -0500

    Modal working

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
-import Link from "next/link";
+import { Modal, ModalBody, ModalContent } from "./ui/animated-modal";
 
 interface ProjectCardProps {
     title: string,
@@ -16,102 +16,109 @@ interface ProjectCardProps {
 }
 
 const ProjectCard = ({ title, desc, bullets, image, link, skills, reverse }: ProjectCardProps) => {
+    const [open, setOpen] = useState(false);
+
     return (
-
-        <CardContainer className="inter-var w-full">
-            <CardBody className="w-full mx-4 md:mx-0 md:w-2/3 bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border  ">
-                <CardItem
-                    as={'p'}
-                    className="text-xl font-bold text-neutral-600 dark:text-white hover:underline cursor-pointer  hover:opacity-60  "
-                    onClick={() => window.open(link, '_blank')}
-                    translateZ={25}
-                >
-                    {title}
-                </CardItem>
-                <div className={`flex ${reverse ? 'xl:flex-row-reverse xl:space-x-reverse' : 'xl:flex-row'} flex-col items-center justify-center xl:space-x-8 xl:space-y-0 space-y-4`}>
+        <>
+            <CardContainer className="inter-var w-full">
+                <CardBody className="w-full mx-4 md:mx-0 md:w-2/3 bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border">
                     <CardItem
-                        className="xl:w-1/2 mt-4 cursor-pointer"
-                        onClick={() => window.open(link, '_blank')}
-                        translateZ={50}
-                    >
-                        <Image
-                            src={image}
-                            height="1040"
-                            width="1800"
-                            className=" object-cover rounded-xl group-hover/card:shadow-xl hover:opacity-60 transition duration-50"
-                            alt="thumbnail"
-                        />
-                    </CardItem>
-                    <div className="flex flex-col justify-between min-h-96 xl:w-1/2 mx-4 space-y-4 ">
-                        <div className="space-y-4">
-                            <CardItem
-                                as="p"
-                                className="text-neutral-500 text-center text-base mt-2 dark:text-neutral-300"
-                            >
-                                {desc}
-                            </CardItem>
-
-
-                            <div>
-                                <CardItem
-                                    as={'p'}
-                                    className="text-lg text-left font-bold text-neutral-600 dark:text-white"
-                                >
-                                    Description:
-                                </CardItem>
-                                <CardItem
-                                    as={'div'}
-                                    className="flex flex-col items-start justify-start"
-                                >
-                                    {bullets.map((bullet, index) => (
-                                        <p key={index} className="text-neutral-500 text-left text-base  md:text-sm my-0.5 dark:text-neutral-300">
-                                            - {bullet}
-                                        </p>
-                                    ))}
-                                
-                                </CardItem>
-                            </div>
-                        </div>
-
-                        <CardItem
-                            as={'div'}
-                            className="flex flex-row flex-wrap justify-center"
-                        >
-                            {skills.map((skill, index) => (
-                                <div
-                                    key={index}
-                                    className="bg-foreground rounded-full px-2.5 py-0.5 m-1"
-                                >
-                                    <p className="text-background text-center font-semibold text-sm ">
-                                        {skill}
-                                    </p>
-                                </div>
-                            ))}
-                        </CardItem>
-                    </div>
-                </div>
-                <div className="flex justify-between items-center mt-8">
-                    <CardItem
-                        as={Link}
-                        href="https://twitter.com/mannupaaji"
-                        target="__blank"
-                        className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white hover:opacity-60 transition duration-50"
-                    >
-                        More info →
-                    </CardItem>
-                    <CardItem
-                        as="button"
-                        className={`px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold  hover:opacity-60 transition duration-50 ${title === 'Visual Snow Log' && 'cursor-not-allowed'}`}
+                        as={'p'}
+                        className="text-xl font-bold text-neutral-600 dark:text-white hover:underline cursor-pointer hover:opacity-60"
                         onClick={() => window.open(link, '_blank')}
                         translateZ={25}
                     >
-                        View Project
+                        {title}
                     </CardItem>
-                </div>
-            </CardBody>
-        </CardContainer>
-
-    )
+                    <div className={`flex ${reverse ? 'xl:flex-row-reverse xl:space-x-reverse' : 'xl:flex-row'} flex-col items-center justify-center xl:space-x-8 xl:space-y-0 space-y-4`}>
+                        <CardItem
+                            className="xl:w-1/2 mt-4 cursor-pointer"
+                            onClick={() => window.open(link, '_blank')}
+                            translateZ={50}
+                        >
+                            <Image
+                                src={image}
+                                height="1040"
+                                width="1800"
+                                className="object-cover rounded-xl group-hover/card:shadow-xl hover:opacity-60 transition duration-50"
+                                alt="thumbnail"
+                            />
+                        </CardItem>
+                        <div className="flex flex-col justify-between min-h-96 xl:w-1/2 mx-4 space-y-4">
+                            <div className="space-y-4">
+                                <CardItem
+                                    as="p"
+                                    className="text-neutral-500 text-center text-base mt-2 dark:text-neutral-300"
+                                >
+                                    {desc}
+                                </CardItem>
+                                <div>
+                                    <CardItem
+                                        as={'p'}
+                                        className="text-lg text-left font-bold text-neutral-600 dark:text-white"
+                                    >
+                                        Description:
+                                    </CardItem>
+                                    <CardItem
+                                        as={'div'}
+                                        className="flex flex-col items-start justify-start"
+                                    >
+                                        {bullets.map((bullet, index) => (
+                                            <p key={index} className="text-neutral-500 text-left text-base md:text-sm my-0.5 dark:text-neutral-300">
+                                                - {bullet}
+                                            </p>
+                                        ))}
+                                    </CardItem>
+                                </div>
+                            </div>
+                            <CardItem
+                                as={'div'}
+                                className="flex flex-row flex-wrap justify-center"
+                            >
+                                {skills.map((skill, index) => (
+                                    <div
+                                        key={index}
+                                        className="bg-foreground rounded-full px-2.5 py-0.5 m-1"
+                                    >
+                                        <p className="text-background text-center font-semibold text-sm">
+                                            {skill}
+                                        </p>
+                                    </div>
+                                ))}
+                            </CardItem>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center mt-8">
+                        <CardItem
+                            onClick={() => setOpen(true)}
+                            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white hover:opacity-60 transition duration-50 cursor-pointer"
+                        >
+                            More info →
+                        </CardItem>
+                        <CardItem
+                            as="button"
+                            className={`px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold hover:opacity-60 transition duration-50 ${title === 'Visual Snow Log' && 'cursor-not-allowed'}`}
+                            onClick={() => window.open(link, '_blank')}
+                            translateZ={25}
+                        >
+                            View Project
+                        </CardItem>
+                    </div>
+                </CardBody>
+            </CardContainer>
+            <Modal open={open} setOpen={setOpen}>
+                <ModalBody open={open} setOpen={setOpen}>
+                    <ModalContent>
+                        <p
+                            className="text-xl font-bold text-neutral-600 dark:text-white hover:underline cursor-pointer hover:opacity-60 text-center"
+                        >
+                            {title} Demo
+                        </p>
+                    </ModalContent>
+                </ModalBody>
+            </Modal>
+        </>
+    );
 }
 
-export default ProjectCard
+export default ProjectCard;
---
+++ components/ui/animated-modal.tsx
@@ -0,0 +1,183 @@
+"use client";
+import { cn } from "@/lib/utils";
+import React, { ReactNode, useEffect, useRef } from "react";
+
+export function Modal({
+  children,
+  className,
+  open,
+  setOpen,
+}: {
+  children: ReactNode;
+  className?: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  const modalRef = useRef(null);
+  useOutsideClick(modalRef, () => setOpen(false));
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Cleanup function to reset overflow when the component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
+  if (!open) return null;
+
+  return (
+    <div className="fixed inset-0 h-full w-full flex items-center justify-center z-50">
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50"
+        onClick={() => setOpen(false)}
+      />
+      <div
+        ref={modalRef}
+        className={cn(
+          "min-h-[50%] max-h-[90%] md:max-w-[40%] bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
+          className
+        )}
+      >
+        <CloseIcon setOpen={setOpen} />
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export const ModalBody = ({
+  children,
+  className,
+  open,
+  setOpen,
+}: {
+  children: ReactNode;
+  className?: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) => {
+  const modalRef = useRef(null);
+  useOutsideClick(modalRef, () => setOpen(false));
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Cleanup function to reset overflow when the component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
+  if (!open) return null;
+
+  return (
+    <div className="fixed inset-0 h-full w-full flex items-center justify-center z-50">
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50"
+        onClick={() => setOpen(false)}
+      />
+      <div
+        ref={modalRef}
+        className={cn(
+          "min-h-[50%] max-h-[90%] md:max-w-[40%] bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
+          className
+        )}
+      >
+        <CloseIcon setOpen={setOpen} />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export const ModalContent = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col flex-1 p-8 md:p-10", className)}>
+      {children}
+    </div>
+  );
+};
+
+export const ModalFooter = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex justify-end p-4 bg-gray-100 dark:bg-neutral-900",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CloseIcon = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
+  return (
+    <button
+      onClick={() => setOpen(false)}
+      className="absolute top-4 right-4 group"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="text-black dark:text-white h-4 w-4 group-hover:scale-125 group-hover:rotate-3 transition duration-200"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M18 6l-12 12" />
+        <path d="M6 6l12 12" />
+      </svg>
+    </button>
+  );
+};
+
+// Hook to detect clicks outside of a component.
+export const useOutsideClick = (
+  ref: React.RefObject<HTMLDivElement>,
+  callback: Function
+) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      callback(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, callback]);
+};
